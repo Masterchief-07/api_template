68dd89c5be8dc27ac894d2c2fce1378a0ec29754
Masterchief-07 <kjonathan1999@hotmail.com>
2023-11-27 15:31:38 +0000

    addig model

--- backend/database/models.go
+++ backend/database/models.go
@@ -1,6 +1,6 @@
 package database
 
 type Example struct {
-	Id int `json:"id" gorm:"type:INT(10) UNSIGNED NOT NULL AUTO_INCREMENT;primary_key`
-	name string `json:"sku"`
+	Id   int    `json:"id" gorm:"type:INT(10) UNSIGNED NOT NULL AUTO_INCREMENT;primary_key`
+	Name string `json:"sku"`
 }
--- backend/main.go
+++ backend/main.go
@@ -1,13 +1,19 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"backend_template/services"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
 	app := fiber.New()
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
-	})
+	// app.Get("/", func(c *fiber.Ctx) error {
+	// 	return c.SendString("Hello, World!")
+	// })
+	app.Get("/", services.HelloWorld)
+	app.Get("/:name", services.HelloCustom)
 
 	app.Listen(":8000")
 }
