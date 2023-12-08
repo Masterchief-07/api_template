51cd99f34f2d6f57327f1f49ed6472d234c674f0
masterchief <kjonathan1999@hotmail.com>
2023-12-08 06:12:43 +0000

    hello world in cpp

--- backend/main.cpp
+++ backend/main.cpp
@@ -2,9 +2,31 @@
 using namespace drogon;
 int main()
 {
-    app().setLogPath("./")
-         .setLogLevel(trantor::Logger::kWarn)
-         .addListener("0.0.0.0", 8001)
-         .setThreadNum(16)
-         .run();
+    app()
+        .registerHandler("/hello", [](const HttpRequestPtr& req,
+                       std::function<void (const HttpResponsePtr &)> &&callback)
+                    {
+                        Json::Value json;
+                        json["result"]=200;
+                        json["message"]=std::string("hello");
+                        auto resp=HttpResponse::newHttpJsonResponse(json);
+                        callback(resp);
+                    },
+                    {Get,"helloworld"})
+        .registerHandler("/hello/{name}", [](const HttpRequestPtr& req,
+                       std::function<void (const HttpResponsePtr &)> &&callback,
+                       const std::string &name)
+                    {
+                        Json::Value json;
+                        json["result"]=200;
+                        json["message"]=std::string("hello, ") + name;
+                        auto resp=HttpResponse::newHttpJsonResponse(json);
+                        callback(resp);
+                    },
+                    {Get,"helloworld"})
+        .setLogPath("./")   
+        .setLogLevel(trantor::Logger::kWarn)
+        .addListener("0.0.0.0", 8001)
+        .setThreadNum(16)
+        .run();
 }
